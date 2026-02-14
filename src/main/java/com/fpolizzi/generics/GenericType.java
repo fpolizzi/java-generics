ab1fb220f4440f153086f9d110315bd634383a8b
Franco Polizzi <me@fpolizzi.com>
2026-02-14 21:16:04 +0100

    feat: add GenericType class

---
+++ src/main/java/com/fpolizzi/generics/GenericType.java
@@ -0,0 +1,35 @@
+package com.fpolizzi.generics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by fpolizzi on 14.02.26
+ */
+public class GenericType {
+
+    static void main() {
+
+        // List<E> (<E> = type parameter) is a generic with the type argument String
+        // only Strings could be in the list
+        List<String> names =
+                new ArrayList<>();
+
+        // List<E> (<E> = type parameter) is a generic with the type argument Integer
+        // only Integers could be in the list
+        List<Integer> numbers =
+                new ArrayList<>();
+
+        // Map<K, V> (<K> = key <V> = value type parameter) is a generic
+        // with the type arguments String and String
+        Map<String, String> map =
+                new HashMap<>();
+
+        // Map<K, V> (<K> = key <V> = value type parameter) is a generic
+        // with the type arguments Integer and Double
+        Map<Integer, Double> map1 =
+                new HashMap<>();
+    }
+}
