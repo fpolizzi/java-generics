8185cb9ba977595a703485c1364f29cac5f0d5f8
Franco Polizzi <me@fpolizzi.com>
2026-02-15 02:45:01 +0100

    feat: add comments to clarify

--- src/main/java/com/fpolizzi/generics/GenericAndMethods.java
+++ src/main/java/com/fpolizzi/generics/GenericAndMethods.java
@@ -7,15 +7,27 @@ public class GenericAndMethods {
 
     static void main() {
 
+<<<<<<< HEAD
+=======
+        // define difference types of arrays
+>>>>>>> 6d57d78 (feat: add comments to clarify)
         String[] names = {"John", "Mary", "Peter"};
         Character[] chars = {'a', '2', 'c'};
         Integer[] numbers = {1, 2, 3};
 
+<<<<<<< HEAD
+=======
+        // invoke the print method with different types
+>>>>>>> 6d57d78 (feat: add comments to clarify)
         print(names);
         print(chars);
         print(numbers);
     }
 
+<<<<<<< HEAD
+=======
+    // use a generic method to perform the same action
+>>>>>>> 6d57d78 (feat: add comments to clarify)
     static <T> void print(T[] array) {
         for (T e : array) {
             System.out.println(
