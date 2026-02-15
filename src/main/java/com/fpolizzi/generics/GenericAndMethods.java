025585bc7f81a70637a95a6b500dcc97a3e3d3f8
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-02-15 04:14:44 +0100

    feat: use of generics (#3)
    * feat: add GenericType class
    
    * feat: use our first generic type
    
    * feat: add generic and methods
    
    * feat: add comments to clarify

--- src/main/java/com/fpolizzi/generics/GenericAndMethods.java
+++ src/main/java/com/fpolizzi/generics/GenericAndMethods.java
@@ -7,15 +7,18 @@ public class GenericAndMethods {
 
     static void main() {
 
+        // define difference types of arrays
         String[] names = {"John", "Mary", "Peter"};
         Character[] chars = {'a', '2', 'c'};
         Integer[] numbers = {1, 2, 3};
 
+        // invoke the print method with different types
         print(names);
         print(chars);
         print(numbers);
     }
 
+    // use a generic method to perform the same action
     static <T> void print(T[] array) {
         for (T e : array) {
             System.out.println(
