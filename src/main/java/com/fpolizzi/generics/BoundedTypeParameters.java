b90bc628692d10370c0b5d72fed8fbe57ce6e346
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-02-15 22:09:19 +0100

    feat: use of bounded type parameters (#4)
    * feat: add BoundedTypeParameters
    
    * feat: add MultipleBounds

---
+++ src/main/java/com/fpolizzi/generics/BoundedTypeParameters.java
@@ -0,0 +1,32 @@
+package com.fpolizzi.generics;
+
+/**
+ * Created by fpolizzi on 15.02.26
+ */
+public class BoundedTypeParameters {
+
+    static void main() {
+
+        Integer[] numbers = {1, 2, 3};
+
+        System.out.println(countGreaterThan(numbers, 1));
+
+        Double[] numbers2 = {1.0, 2.0, 3.0};
+
+        System.out.println(countGreaterThan(numbers2, 1.0));
+    }
+
+    static <T extends Comparable<T>> int countGreaterThan(T[] numbers, T number) {
+
+        int count = 0;
+
+        for (T n : numbers) {
+
+            if (n.compareTo(number) > 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
