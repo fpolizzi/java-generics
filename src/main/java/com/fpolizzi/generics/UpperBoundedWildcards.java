25f389a07e17ffda266c2e6f540ca98199f37dcd
Franco Polizzi <me@fpolizzi.com>
2026-02-16 22:40:46 +0100

    feat: add UpperBoundedWildcards

---
+++ src/main/java/com/fpolizzi/generics/UpperBoundedWildcards.java
@@ -0,0 +1,31 @@
+package com.fpolizzi.generics;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by fpolizzi on 16.02.26
+ */
+public class UpperBoundedWildcards {
+
+    static void main() {
+
+        // Upper Bounded Wildcards
+        List<Double> list1 = Arrays.asList(0.6, 1.5, 0.9);
+        List<Integer> list2 = Arrays.asList(0, 1, 2);
+        List<Number> list3 = Arrays.asList(100, 4.7, -0.45);
+
+        print(list1);
+        print(list2);
+        print(list3);
+    }
+
+    // everything that extends Number is allowed -> upper bound wildcard
+    static void print(List<? extends Number> list) {
+
+        list.forEach(e -> {
+            System.out.println(e.getClass().getName());
+            System.out.println(e);
+        });
+    }
+}
