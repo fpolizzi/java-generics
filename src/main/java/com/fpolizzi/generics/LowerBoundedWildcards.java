6f8694196b6302d4b6794a08f4fbe591b01c5800
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-02-16 23:09:40 +0100

    feat: use wildcards (#5)
    * feat: add UnboundedWildcards
    
    * feat: add UpperBoundedWildcards
    
    * feat: add LowerBoundedWildcards

---
+++ src/main/java/com/fpolizzi/generics/LowerBoundedWildcards.java
@@ -0,0 +1,31 @@
+package com.fpolizzi.generics;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by fpolizzi on 16.02.26
+ */
+public class LowerBoundedWildcards {
+
+    static void main() {
+
+        // Lower Bounded Wildcards
+        List<Number> list1 = Arrays.asList(0.3, 1.5, 0.8);
+        List<Integer> list2 = Arrays.asList(0, 1, 2);
+        List<Number> list3 = Arrays.asList(0, 1, 2);
+
+        print(list1);
+        print(list2);
+        print(list3);
+    }
+
+    // every super class from Integer(included) is allowed -> lower bound wildcard
+    static void print(List<? super Integer> list) {
+
+        list.forEach(e -> {
+            System.out.println(e.getClass().getName());
+            System.out.println(e);
+        });
+    }
+}
---
+++ src/main/java/com/fpolizzi/generics/UnboundedWildcards.java
@@ -0,0 +1,31 @@
+package com.fpolizzi.generics;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by fpolizzi on 16.02.26
+ */
+public class UnboundedWildcards {
+
+    static void main() {
+
+        // Unbound Wildcards
+        List<Number> list1 = Arrays.asList(0.3, 1.5, 0.8);
+        List<Integer> list2 = Arrays.asList(0, 1, 2);
+        List<String> list3 = Arrays.asList("0", "1", "2");
+
+        print(list1);
+        print(list2);
+        print(list3);
+    }
+
+    // the '?' is a wildcard type that represents any type -> unbound wildcard
+    static void print(List<?> list) {
+
+        list.forEach(e -> {
+            System.out.println(e.getClass().getName());
+            System.out.println(e);
+        });
+    }
+}
